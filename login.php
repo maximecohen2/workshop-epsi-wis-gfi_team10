fb5d830cc5ac071e691ccaebd6c3186f5e4b4fef
Maxime Cohen <maxime.cohen-pro@outlook.fr>
2017-10-03 17:45:33 +0200

    ajout de la vue login

---
+++ login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Album example for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/album.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-dark bg-dark">
+      <div class="container d-flex justify-content-between">
+        <a href="#" class="navbar-brand">Login</a>
+      </div>
+    </div>
+
+    <section class="jumbotron text-center">
+      <div class="container">
+		  <form>
+    		<div class="form-group">
+      			<label for="exampleInputEmail1">Email address</label>
+				<div class="input-group">
+  					<input type="text" class="form-control" placeholder="username" aria-describedby="basic-addon2">
+  					<span class="input-group-addon" id="basic-addon2">@gfi.fr</span>
+				</div>
+		    	<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    		</div>
+			<button type="submit" class="btn btn-primary">Sign in</button>
+		</form>
+      </div>
+    </section>
+
+	<!--
+    <footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+      </div>
+    </footer>
+	-->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../js/jquery-3.2.1.min.js"></script>
+    <script src="../js/popper.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
