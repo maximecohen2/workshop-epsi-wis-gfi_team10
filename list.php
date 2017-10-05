d5227aba581a24120f6301a244b9d9b4071a52b7
Maxime Cohen <maxime.cohen-pro@outlook.fr>
2017-10-05 15:21:40 +0200

    maj list.php

--- list.php
+++ list.php
@@ -5,7 +5,7 @@
 # @Project: Snake
 # @Filename: list.php
 # @Last modified by:   maxime
-# @Last modified time: 2017-Oct-04 18:00
+# @Last modified time: 2017-Oct-05 15:14
 require "./models/model_list.php";
 ?>
 
@@ -26,6 +26,7 @@ require "./models/model_list.php";
     <link href="./css/album.css" rel="stylesheet">
 
 	<link rel="stylesheet" type="text/css" href="./css/dataTables.bootstrap4.min.css"/>
+	<link rel="stylesheet" type="text/css" href="./css/responsive.bootstrap4.min.css"/>
   </head>
 
   <body>
@@ -42,7 +43,8 @@ require "./models/model_list.php";
     <div class="navbar navbar-dark bg-dark">
       	<div class="container d-flex justify-content-between">
         	<a href="#" class="navbar-brand">Liste des besoins</a>
-        	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader"
+			aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
           		<span class="navbar-toggler-icon"></span>
         	</button>
       	</div>
@@ -50,43 +52,30 @@ require "./models/model_list.php";
 
     <section class="jumbotron">
 	  	<div class="container">
-		  	<div class="py-3">
-			  	<table id="table" class="table table-striped table-bordered">
-			    	<thead>
-			      		<tr>
-			        		<th>Statut</th>
-			        		<th>Titre</th>
-					        <th>Date</th>
-							<th>Client</th>
-			      		</tr>
-			    	</thead>
-					<tbody>
-						<?php
-						foreach ($data as $line) {
-							echo "<tr>\n";
-							foreach ($line as $elem) {
-								echo "<td>".$elem."</td>\n";
-							}
-							echo "</tr>\n";
+			<table id="table" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+				<thead>
+			  		<tr>
+			    		<th>Statut</th>
+			    		<th>Titre</th>
+				        <th>Date</th>
+						<th>Client</th>
+			  		</tr>
+			   	</thead>
+				<tbody>
+					<?php
+					foreach ($data as $line) {
+						echo "<tr>\n";
+						foreach ($line as $elem) {
+							echo "<td>".$elem."</td>\n";
 						}
-						?>
-					</tbody>
-				</table>
-		  	</div>
+						echo "</tr>\n";
+					}
+					?>
+				</tbody>
+			</table>
 		</div>
     </section>
 
-	<!--
-    <footer class="text-muted">
-      <div class="container">
-        <p class="float-right">
-          <a href="#">Back to top</a>
-        </p>
-        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
-      </div>
-    </footer>
-	-->
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
@@ -95,9 +84,29 @@ require "./models/model_list.php";
     <script src="./js/bootstrap.min.js"></script>
 	<script src="./js/jquery.dataTables.min.js"></script>
 	<script src="./js/dataTables.bootstrap4.min.js"></script>
+	<script src="./js/dataTables.responsive.min.js"></script>
+	<script src="./js/responsive.bootstrap4.min.js"></script>
+	<script src="./js/date-eu.js"></script>
 	<script>
 	$(document).ready(function() {
-    $('#table').DataTable();
+		$.fn.dataTable.ext.type.order['statut-pre'] = function ( d ) {
+    		switch ( d ) {
+        		case 'open':    return 1;
+        		case 'win': return 2;
+        		case 'lost':   return 3;
+    		}
+    		return 0;
+		};
+	    $('#table').DataTable( {
+			"lengthChange": false,
+			"pageLength": 5,
+	        "info":     false,
+			"columnDefs": [
+	   			{ "type": "statut", "targets": 0 },
+				{ "type": "date-eu", "targets": 2 }
+	 		],
+			"order": [["0", "asc"], [2, "desc"]]
+		} );
 	} );
 	</script>
   </body>
--- models/model_list.php
+++ models/model_list.php
@@ -5,24 +5,24 @@
 # @Project: Snake
 # @Filename: getData.php
 # @Last modified by:   maxime
-# @Last modified time: 2017-Oct-04 17:36
+# @Last modified time: 2017-Oct-05 10:36
 
 
 $data = array(
 	array('statut'=>"open", 'titre'=>"lol", 'date'=>"04/10/2017", 'client'=>"Patrick"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
-	array('statut'=>"close", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals")
+	array('statut'=>"open", 'titre'=>"Bitume", 'date'=>"05/10/2017", 'client'=>"Vals"),
+	array('statut'=>"open", 'titre'=>"Bitume", 'date'=>"10/01/2017", 'client'=>"Vals"),
+	array('statut'=>"win", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"win", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"win", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"win", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"win", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"win", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"open", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"open", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"open", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"lost", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"lost", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"lost", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals"),
+	array('statut'=>"lost", 'titre'=>"Bitume", 'date'=>"29/03/2017", 'client'=>"Vals")
 	);
